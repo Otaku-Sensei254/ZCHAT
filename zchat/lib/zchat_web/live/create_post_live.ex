a0639879149e2aafd7613d1254c2a619a2684d09
Dtech <dhuhuinc@gmail.com>
2026-03-22 12:44:37 +0300

    changed from 30mb to 50mb

--- zchat/lib/zchat_web/live/create_post_live.ex
+++ zchat/lib/zchat_web/live/create_post_live.ex
@@ -27,7 +27,7 @@ defmodule ZchatWeb.CreatePostLive do
      |> allow_upload(:media,
        accept: ~w(.jpg .jpeg .png .gif .mp4 .mov .webp),
        max_entries: 20,
-       max_file_size: 20_000_000,
+       max_file_size: 50_000_000,
        auto_upload: true,
        progress: &handle_progress/3
      )}
@@ -87,7 +87,6 @@ defmodule ZchatWeb.CreatePostLive do
 
   @impl true
   def handle_event("save", %{"post" => post_params}, socket) do
-    # 1. Use files that were already consumed and stored in assigns
     clean_media =
       socket.assigns.uploaded_files
       |> Enum.map(fn file -> Map.take(file, ["url", "type"]) end)
@@ -163,7 +162,7 @@ defmodule ZchatWeb.CreatePostLive do
     {:noreply, assign(socket, changeset: new_changeset, form: to_form(new_changeset, as: :post))}
   end
 
-  def error_to_string(:too_large), do: "File is too large (Max 20MB)"
+  def error_to_string(:too_large), do: "File is too large (Max 50MB)"
   def error_to_string(:too_many_files), do: "You have selected too many files"
   def error_to_string(:not_accepted), do: "You have selected an unacceptable file type"
 end

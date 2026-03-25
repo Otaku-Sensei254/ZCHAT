c312102ecdd9e15d2964d3c201f69b8af1774f7d
Dtech <dhuhuinc@gmail.com>
2026-03-25 18:35:00 +0300

    fixed UI from Ed, points and added the store yup again

--- lib/vibeflow_web/live/UI/Store/store_live.ex
+++ lib/vibeflow_web/live/UI/Store/store_live.ex
@@ -3,16 +3,26 @@ defmodule VibeflowWeb.UI.Store.StoreLive do
 
   @impl true
   def mount(_params, _session, socket) do
+    points_balance =
+      socket.assigns.current_user
+      |> maybe_get_points()
+
+    socket =
+      socket
+      |> assign(points_balance: points_balance)
+      |> assign(points_display: format_points(points_balance))
+
     {:ok, socket}
   end
 
-  # @impl true
-  # def render(assigns) do
-  #   ~H"""
-  #   <div>
-  #       <h1>Hello</h1>
+  defp maybe_get_points(nil), do: 0
+  defp maybe_get_points(%{points: points}) when is_integer(points), do: points
+  defp maybe_get_points(_), do: 0
 
-  #   </div>
-  #   """
-  # end
+  defp format_points(points) when is_integer(points) do
+    points
+    |> Integer.to_string()
+    |> String.replace(~r/(?<=\d)(?=(?:\d{3})+$)/, ",")
+  end
+  defp format_points(_), do: "0"
 end
